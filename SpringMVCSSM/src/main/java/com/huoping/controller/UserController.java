b9247ab3f40f228e6687e51c755c677b5b51b8fc
huoping47 <huoping_47@126.com>
2022-07-27 18:35:37 +0800

    SpringSSM整合User项目

--- SpringMVCSSM/src/main/java/com/huoping/controller/UserController.java
+++ SpringMVCSSM/src/main/java/com/huoping/controller/UserController.java
@@ -3,9 +3,9 @@ package com.huoping.controller;
 import com.huoping.pojo.User;
 import com.huoping.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -14,7 +14,9 @@ import java.util.List;
  *霍平
  *2022/7/25 25 20:52
  */
-@Controller
+//@Controller
+@CrossOrigin   //在服务器端支持跨域访问
+@RestController  //如果本类中都是ajax请求,那么可以使用RestController这个注解,方法体上的@ResponseBody可以不写
 @RequestMapping("/user")
 public class UserController {
     //界面层一定会有数据访问层的对象
@@ -24,7 +26,7 @@ public class UserController {
 
     ///user/selectUserPage?userName=z&userSex=男&page=null
     @RequestMapping("/selectUserPage")
-    @ResponseBody
+//    @ResponseBody
     public List<User> selectUserPage(String userName, String userSex, Integer page) {
         int startRow = 0;
         if (page != null) {
@@ -35,18 +37,25 @@ public class UserController {
 
     //根据用户ID删除用户	url	/user/ deleteUserById?userId= 15968162087363060
     @RequestMapping("/deleteUserById")
-    @ResponseBody
+//    @ResponseBody
     public int deleteUserById(String userId) {
         return userService.deleteUserById(userId);
     }
 
     //增加用户	url	/user/createUser(参数见下面)
     @RequestMapping("createUser")
-    @ResponseBody
+//    @ResponseBody
     public int createUser(User user) {
         String Uuid = System.currentTimeMillis() + "";
         user.setUserId(Uuid);
         return userService.createUser(user);
     }
 
+    //获取总行数	url	/user/getRowCount?userName=z&userSex=男
+    @RequestMapping("/getRowCount")
+//    @ResponseBody
+    public int getRowCount(String userName, String userSex) {
+        return userService.getRowCount(userName, userSex);
+
+    }
 }
